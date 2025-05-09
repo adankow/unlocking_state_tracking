3464125cab5c426bce51e56ddf687eec04d6fa80
Adam <adankowiakowski02@gmail.com>
2025-05-09 13:56:36 +0100

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ pip install dacite
 pip install omegaconf
 pip install torchmetrics
 
-git clone https://github.com/automl/unlocking_state_tracking.git
-
 cd unlocking_state_tracking/flash-linear-attention_mod/; pip install -e .
 cd unlocking_state_tracking/mamba_dev/; pip install -e . --no-cache-dir
