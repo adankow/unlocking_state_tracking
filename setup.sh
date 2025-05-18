d15c2e893afcca81bc9491bf2fb0b08812f08bc5
Adam <adankowiakowski02@gmail.com>
2025-05-18 01:51:25 +0100

    Add FFLM experiments

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-#!\bin\bash
+#!/bin/bash
+set -e
+
 
 pip install causal-conv1d>=1.4.0
 pip install dacite
 pip install omegaconf
 pip install torchmetrics
 
-cd unlocking_state_tracking/flash-linear-attention_mod/; pip install -e .
-cd unlocking_state_tracking/mamba_dev/; pip install -e . --no-cache-dir
+cd flash-linear-attention_mod/; pip install -e .
+cd ../mamba_dev/; pip install -e . --no-cache-dir
